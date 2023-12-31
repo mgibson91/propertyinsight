6be32d9e661b532ae7b8ce1097fe4da1a148ed64
Matt <matthewgibson91@gmail.com>
2023-12-31 13:19:07 +0000

    Add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import "../css/preflight-without-button.css";
-import "../css/globals.css" ;
-import "@radix-ui/themes/styles.css";
+import '../css/preflight-without-button.css';
+import '../css/globals.css';
+import '@radix-ui/themes/styles.css';
+import { MatchingSnapshotProvider } from '@/app/matching-snapshot-provider';
+import { NavBar } from '@/app/nav-bar';
+import { DisplayModeAwareRadixThemeProvider } from '@/app/display-mode-aware-radix-theme-provider';
+import Script from 'next/script';
 
-import { Heading, Theme } from "@radix-ui/themes";
-import { MatchingSnapshotProvider } from "@/app/matching-snapshot-provider";
-import { NavBar } from "@/app/nav-bar";
-import { DisplayModeAwareRadixThemeProvider } from "@/app/display-mode-aware-radix-theme-provider";
-
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
@@ -24,6 +21,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.GTM_ID}');
+          `}
+    </Script>
     <body className="">
       <DisplayModeAwareRadixThemeProvider>
         <main className="min-h-screen flex flex-col items-center">
