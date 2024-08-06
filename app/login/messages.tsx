ef349be19a36fbdf1f6d82d545474e6fe4242f39
Matt <matthewgibson91@gmail.com>
2024-08-06 06:49:52 +0100

    Update signup / login error messages

--- app/login/messages.tsx
+++ app/login/messages.tsx
@@ -1,23 +1,32 @@
-'use client'
+'use client';
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
+import { Card } from '@radix-ui/themes';
 
 export default function Messages() {
-  const searchParams = useSearchParams()
-  const error = searchParams.get('error')
-  const message = searchParams.get('message')
+  const searchParams = useSearchParams();
+  const error = searchParams?.get('error');
+  const message = searchParams?.get('message');
   return (
-    <>
-      {error && (
-        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
-          {error}
-        </p>
-      )}
-      {message && (
-        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
-          {message}
-        </p>
-      )}
-    </>
-  )
+    <div className="mt-5">
+      {error ||
+        (message && (
+          <Card variant={'surface'}>
+            {error && <p className="text-[var(--tomato-11)]">{error}</p>}
+            {message && <p className="">{message}</p>}
+
+            {message == 'Could not authenticate user' && (
+              <div className={'flex flex-col items-start mt-3'}>
+                <p className="">You'll need to either:</p>
+                <p className="">&bull; Log in instead of sign up</p>
+                <p className="">&bull; Verify your email if you've already signed up</p>
+                <p className="text-sm italic">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&bull; Signup again to resend verification email
+                </p>
+              </div>
+            )}
+          </Card>
+        ))}
+    </div>
+  );
 }
