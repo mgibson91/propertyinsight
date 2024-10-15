093672a9c38dd67ab0de720bfe3734b1befc7806
Matt <matthewgibson91@gmail.com>
2024-10-15 09:57:26 +0100

    Feedback

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,20 @@ import '../css/globals.css';
 import '@radix-ui/themes/styles.css';
 import React from 'react';
 import { DisplayModeAwareRadixThemeProvider } from '@/app/display-mode-aware-radix-theme-provider';
+import FeedbackForm from '@/app/(authenticated)/feedback/FeedbackForm';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="">
-        <DisplayModeAwareRadixThemeProvider>{children}</DisplayModeAwareRadixThemeProvider>
+        <DisplayModeAwareRadixThemeProvider>
+          <main className="flex-auto flex flex-col items-center">
+            {children}
+            <div className="w-full bg-accent-bg border-t border-primary-line">
+              <FeedbackForm />
+            </div>
+          </main>
+        </DisplayModeAwareRadixThemeProvider>
       </body>
     </html>
   );
